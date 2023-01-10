31366e6048792157b03bbefa3879bd0202b9c92f
Huntney1 <devilwhiteone@hotmail.it>
2023-01-10 19:41:37 +0100

    fix stile

--- vite-hello-world/src/App.vue
+++ vite-hello-world/src/App.vue
@@ -12,17 +12,33 @@ export default {
 
 <template>
   <div class="bordi">
-    <TitleApp />
+    <p class="title">
+      <TitleApp />
+    </p>
+
     <AppHero />
   </div>
 </template>
 
 <style scope>
-.bordi{
+.bordi {
   width: 15em;
   height: 5em;
-  padding-top: 1.5em;
   border: 2px solid white;
+  
+}
+.bordi:hover{
+  display: none;
+  background-color: rgb(128, 4, 162);
+}
 
+.bordi:hover {
+  color: aquamarine;
+  border: 3px solid aquamarine;
+  display: block;
+}
+
+.title:hover {
+  text-shadow: 2px 2px 5px blue;
 }
 </style>
