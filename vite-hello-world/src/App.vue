ca0d556dddb85c34824d6b4782434bfc2dc5a49b
Huntney1 <devilwhiteone@hotmail.it>
2023-01-10 14:45:33 +0100

    1° mileston fatta

---
+++ vite-hello-world/src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import appTitle from "./components/AppTitle.vue";
+/* import jumbo from "./components/jumbo.vue"; */
+
+export default {
+  components: {
+    appTitle,
+   /*  jumbo, */
+  },
+};
+</script>
+
+<template>
+  <div>
+    <appTitle />
+    <Jumbo />
+  </div>
+</template>
+
+<style scope>
+</style>
---
+++ vite-hello-world/src/components/appTitle.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: "appTitle",
+  data() {
+    return {
+      hello: "La Mia Prima App Con Vite!",
+    };
+  },
+  methods: {},
+};
+</script>
+
+<template lang="">
+  {{hello}}
+</template>
+
+<style>
+</style>
